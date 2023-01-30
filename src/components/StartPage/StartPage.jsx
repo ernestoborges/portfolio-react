9fd572c34b0103d9c8fea264558b61e68ef11122
Ernesto Neto <ernestoborges1995@gmail.com>
2023-01-30 18:23:44 -0300

    feat: Finished basic login screenand styling

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,7 +1,30 @@
+import "./styles.css"
 import {useNavigate} from "react-router-dom"
+import { DateTime } from "../Header/DateTime";
+import { useState } from "react";
+
 export function StartPage(){
     const navigate = useNavigate();
-    return <div>
-        <button onClick={()=>navigate("/pc")}>login</button>
-    </div>
+    const [isUserSelected, setIsUserSelected] = useState(false);
+    return (
+        <article className="start-page">
+            <header>
+                <DateTime />
+            </header>
+            <section className="login-window">
+                <h2>Select User</h2>
+                <ul className="user-list"> 
+                    <li className={`user ${isUserSelected ? "user-selected" : ""}`} onClick={()=>setIsUserSelected(!isUserSelected)}>
+                        <img src="/images/profile/profile-pic.png" alt=""/>
+                        <span>Ernesto Borges</span>
+                    </li>
+                </ul>
+                <button onClick={()=>navigate("/pc")} disabled={!isUserSelected}>Enter</button>
+            </section>
+            <footer>
+                Portfolio
+            </footer>
+        </article>
+
+    )
 }
